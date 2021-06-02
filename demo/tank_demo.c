161929c59934b88261a00140897fbf68c37caa10
Rajeev Datta <rajeevlives@gmail.com>
2021-06-01 23:23:04 -0400

    stuff

--- demo/tank_demo.c
+++ demo/tank_demo.c
@@ -46,8 +46,8 @@ static const SDL_Color BLACK_TEXT = {0, 0, 0};
 static const SDL_Color MAROON_TEXT = {128, 0, 0};
 static const int LEVEL_BUFFER = 50;
 static const int CIRC_PTS = 16;
-static const double FORCE_FIELD_INNER_RADIUS = 7;
-static const double FORCE_FIELD_OUTER_RADIUS = 8;
+static const double FORCE_FIELD_INNER_RADIUS = 60;
+static const double FORCE_FIELD_OUTER_RADIUS = 70;
 static const double FORCE_FIELD_MASS = 50;
 static const double IMG_X_SCALE = 0.9;
 static const double IMG_Y_SCALE = 0.8;
@@ -135,37 +135,38 @@ void on_key_push(char key, key_event_type_t type, double held_time,
         //         tank1_body = scene_get_body(scene, i);
         //     }
         // }
-        // if (tank_get_weapon(tank1) == (shooting_handler_t) remote_missile_shoot) {
-        //     for (size_t i=7; scene_bodies(scene); i++) {
-        //         if (*(body_types_t *) body_get_info(scene_get_body(scene, i)) == TANK_REMOTE_MISSILE) {
-        //             body_t *missile = scene_get_body(scene, i);
 
-        //             if (type == KEY_RELEASED) {
-        //                 body_set_velocity(missile, (vector_t){0, 0});
-        //             } else if (type == KEY_PRESSED) {
-        //                 vector_t speed = {0, 0};
-        //                 switch (key) {
-        //                     case DOWN_ARROW:
-        //                         speed.x = -INIT_VEL * cos(body_get_orientation(missile));
-        //                         speed.y = -INIT_VEL * sin(body_get_orientation(missile));
-        //                         break;
-        //                     case UP_ARROW:
-        //                         speed.x = INIT_VEL * cos(body_get_orientation(missile));
-        //                         speed.y = INIT_VEL * sin(body_get_orientation(missile));
-        //                         break;
-        //                     case RIGHT_ARROW:
-        //                         body_set_rotation(missile, body_get_orientation(missile) + ANGLE_OFFSET);
-        //                         break;
-        //                     case LEFT_ARROW:
-        //                         body_set_rotation(missile, body_get_orientation(missile) - ANGLE_OFFSET);
-        //                         break;
-        //                 }
-        //                 body_set_velocity(missile, speed);
-        //             }
-        //         }
-        //     }
-        // }
-        // else {
+        if (tank_get_weapon(tank1) == (shooting_handler_t) remote_missile_shoot) {
+            for (size_t i=0; scene_bodies(scene); i++) {
+                if (*(body_types_t *) body_get_info(scene_get_body(scene, i)) == TANK_REMOTE_MISSILE) {
+                    body_t *missile = scene_get_body(scene, i);
+
+                    if (type == KEY_RELEASED) {
+                        body_set_velocity(missile, (vector_t){0, 0});
+                    } else if (type == KEY_PRESSED) {
+                        vector_t speed = {0, 0};
+                        switch (key) {
+                            case DOWN_ARROW:
+                                speed.x = -INIT_VEL * cos(body_get_orientation(missile));
+                                speed.y = -INIT_VEL * sin(body_get_orientation(missile));
+                                break;
+                            case UP_ARROW:
+                                speed.x = INIT_VEL * cos(body_get_orientation(missile));
+                                speed.y = INIT_VEL * sin(body_get_orientation(missile));
+                                break;
+                            case RIGHT_ARROW:
+                                body_set_rotation(missile, body_get_orientation(missile) + ANGLE_OFFSET);
+                                break;
+                            case LEFT_ARROW:
+                                body_set_rotation(missile, body_get_orientation(missile) - ANGLE_OFFSET);
+                                break;
+                        }
+                        body_set_velocity(missile, speed);
+                    }
+                }
+            }
+        }
+        else {
             if (type == KEY_RELEASED) {
                 body_set_velocity(tank1_body, (vector_t){0, 0});
             } else if (type == KEY_PRESSED) {
@@ -186,10 +187,7 @@ void on_key_push(char key, key_event_type_t type, double held_time,
                         body_set_rotation(tank1_body, body_get_orientation(tank1_body) - ANGLE_OFFSET);
                         break;
                     case ' ':
-                        printf("tank time: %f\n", body_get_time(tank1_body));
-                        printf("tank reload time: %f\n", tank_get_curr_reload(tank1));
                         if (body_get_time(tank1_body) > tank_get_curr_reload(tank1)) {
-                            printf("shoots!\n");
                             tank_shoot(scene, tank1);
                             tank_set_body_time(tank1, 0);
                         }
@@ -197,7 +195,7 @@ void on_key_push(char key, key_event_type_t type, double held_time,
                 }
                 body_set_velocity(tank1_body, speed);
             }
-        // }
+        }
     }
 }
 

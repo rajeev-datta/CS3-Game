8c49e774f1ee1050e511a112cf78f696a98b8dad
aditee <aditee@caltech.edu>
2021-06-05 12:51:12 -0700

    Sound effects prep

---
+++ demo/sound_tester.c
@@ -0,0 +1,12 @@
+#include "sdl_wrapper.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+
+int main() {
+    Mix_Chunk* effect1;
+    Mix_Chunk* effect2;
+   // Mix_Music music;
+}

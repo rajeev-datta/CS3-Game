0537da7d6d79a4d95a6a1c79b2cdc0750665f840
aditee <aditee@caltech.edu>
2021-06-06 18:33:25 -0700

    MuSiC iS cOoL

--- demo/sound_tester.c
+++ demo/sound_tester.c
@@ -6,7 +6,17 @@
 #include <SDL2/SDL_mixer.h>
 
 int main() {
-    Mix_Chunk* effect1;
-    Mix_Chunk* effect2;
-   // Mix_Music music;
+    SDL_Init(SDL_INIT_AUDIO);
+    Mix_Init(MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MP3 | MIX_INIT_OGG);
+    
+    Mix_Chunk* remove_sound;
+    Mix_Chunk* shoot_sound;
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+    remove_sound = Mix_LoadWAV("remove_sound.wav");
+    shoot_sound = Mix_LoadWAV("shoot_sound.wav");
+    Mix_PlayChannel(-1, remove_sound, 0);
+    Mix_PlayChannel(-1, shoot_sound, 0);
+
+    Mix_FreeChunk(remove_sound);
+    Mix_FreeChunk(shoot_sound);
 }

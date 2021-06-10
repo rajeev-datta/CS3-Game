9280f7952c7ca2dee20d94d257c2352192f66a29
Rajeev Datta <rajeevlives@gmail.com>
2021-06-09 23:07:12 -0400

    random power ups

--- demo/tank_demo.c
+++ demo/tank_demo.c
@@ -978,8 +978,8 @@ int main(int argc, char *argv[]) {
 
             // Shoot a power-up at an interval of time.
             if (time_passed > TANK_POWER_UP_TIME) {
-                // make_tank_power_up(temp_scene, rand() % NUM_POWERUPS, tank1, tank2);
-                make_tank_power_up(temp_scene, 3, tank1, tank2);
+                make_tank_power_up(temp_scene, rand() % NUM_POWERUPS, tank1, tank2);
+                // make_tank_power_up(temp_scene, 3, tank1, tank2);
                 time_passed = 0;
             }
         } else {
